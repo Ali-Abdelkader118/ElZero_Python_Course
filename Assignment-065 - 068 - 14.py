ee55ce502ef063b135733335d4d11157279e41a9
Ali-Abdelkader118 <86664989+Ali-Abdelkader118@users.noreply.github.com>
2021-09-13 00:31:12 +0200

    Update Assignment-065 - 068 - 14.py

--- Assignment-065 - 068 - 14.py
+++ Assignment-065 - 068 - 14.py
@@ -1,29 +1,60 @@
 import os
-#First
+# First
 num = 1
 
-
-while num <= 50 :
-    if num == 25 :
-        file = open("D:\Python\Python-14\special-text.txt" , "w")
-    else :
-        file = open(fr"D:\Python\Python-14\text{str(num)}.txt" , "w")
+while num <= 50:
+    if num == 25:
+        file = open("D:\Python\Python-14\special-text.txt", "w")
+    else:
+        file = open(fr"D:\Python\Python-14\text{str(num)}.txt", "w")
         file.write(f"Elzero Web School => {num}")
-    num +=1
+    num += 1
     file.close()
-    
+
 print(os.getcwd())      # Current Working Directory
 print(os.path.dirname(os.path.abspath(__file__)))      # The Path of My Folder
-print(os.path.basename(file.name))  
-#Second 
-file = open(r"D:\Python\Python-14\text1.txt" , "a")
+print(os.path.basename(file.name))
+print(num)
 
-file.write("\nAppended => Elzero Web School \n" * 50)
+print("=" * 40)
+# Second
+file = open(r"D:\Python\Python-14\text1.txt", "a")
+
+file.write("\n Appended => Elzero Web School " * 50)
+file.close()
+
+# Third
+text_file = open(r"D:\Python\Python-14\text1.txt", "r")
+number_of_lines = 0
+number_of_words = 0
+number_of_characters = 0
+num_of_l = 0
+for line in text_file :
+    line = line.strip("\n")
+    words = line.split()
+    number_of_lines += 1
+    number_of_words += len(words)
+    number_of_characters += len(line)
+    if words =="l":
+        num_of_l +=1
 
 
-#Third
+def letter_counter(letter) :
+    text_file = open(r"D:\Python\Python-14\text1.txt", "r")
+    text =text_file.read()
+    return text.count(letter)
+
+text_file.close()
+
+print(f"Number Of Lines Is => {number_of_lines}")
+print(f"Number Of Words Is => {number_of_words}")
+print(f"Number Of Chars Is => {number_of_characters}")
+print(f"Number Of \"l\" Char Is => {letter_counter('l')}")
 
 
-#Fourth
-for num4 in range(41 ,51) :
+text_file.close()
+
+print("=" * 40)
+# Fourth
+for num4 in range(41, 51):
     os.remove(fr"D:\Python\Python-14\text{num4}.txt")
