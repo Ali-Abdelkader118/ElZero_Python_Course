d4e33130e076ba3e5134d2794debd131eeea182d
Ali-Abdelkader118 <86664989+Ali-Abdelkader118@users.noreply.github.com>
2021-09-16 17:48:54 +0200

    Update Assignment-086 - 089 - 20.py

--- Assignment-086 - 089 - 20.py
+++ Assignment-086 - 089 - 20.py
@@ -30,7 +30,22 @@ print(final_string)
 print("=" * 40)
 
 #Second
-# Still Solving
+my_list1 = ["E", "L", "Z", "E", "R", "O", 1, 2]
+my_tuple = ("E", "Z", "R", 1, 2, "E", "R", "O")
+my_list2 = ("L", "E", "O", 1, 2, "E", "R", "O")
+my_data = []
+
+for item1, item2, item3 in zip(my_list1, my_tuple, my_list2):
+    if type(item2) == int :
+        break
+    elif type(item3) == int :
+        break
+    else :
+        finall_string =item2,item3
+
+
+print(final_string)
+
 
 print("=" * 40)
 
