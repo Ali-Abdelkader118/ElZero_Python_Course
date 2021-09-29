ffda92520b29d66f878c2001802a352c0f8c8299
Ali-Abdelkader118 <86664989+Ali-Abdelkader118@users.noreply.github.com>
2021-09-29 17:32:28 +0200

    Update Assignment-095 - 102 - 5.py

--- Assignment-095 - 102 - 5.py
+++ Assignment-095 - 102 - 5.py
@@ -1,1 +1,21 @@
+#First
 
+#Second
+
+#[L](Elzero)
+
+#Third
+
+#(\+)?(\(0100\))?\s\d{2,4}(\-)\d{4}
+
+#Fourth
+
+#(https?)://(www)?\.?(\w+):?(\d+)?/?(.com|.org)(\S+)
+
+#Fifth
+
+# 1) (https?)
+
+# 2) [h-t]
+
+# 3) 
