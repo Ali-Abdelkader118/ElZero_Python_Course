e3345128cbe4ddcffb43c3891b99763bc9cb1288
Ali-Abdelkader118 <86664989+Ali-Abdelkader118@users.noreply.github.com>
2021-11-09 22:40:08 +0200

    Update Assignment-103 - 116 - 23.py

--- Assignment-103 - 116 - 23.py
+++ Assignment-103 - 116 - 23.py
@@ -6,11 +6,159 @@ class Game:
         self.year = year
         self.price = price
     def price_in_pounds(self) :
-        return f"{self.price * 15.5} Egyptian Pounds"
+        return f"{self.price * 15.6} Egyptian Pounds"
 
 game_one = Game("Ys", "Falcom", 2010, 50)
 
 print(f"Game Name Is \"{game_one.name}\", ", end="")
 print(f"Developer Is \"{game_one.developer}\", ", end="")
 print(f"Release Date Is \"{game_one.year}\", ", end="")
-print(f"Price In Egypt Is {game_one.price_in_pounds()}", end="")
+print(f"Price In Egypt Is {game_one.price_in_pounds()}")
+
+
+#Second
+class User:
+    def __init__(self , fname, lname, age, gender) :
+        self.fname = fname
+        self.lname = lname[0]
+        self.age = age
+        self.gender = gender
+    def full_details(self) :
+        if self.gender == "Male" :
+            return f"Hello Mr {self.fname} {self.lname}. [{str(40 - self.age).zfill(2)}] Years To Reach 40"
+        elif self.gender == "Female" :
+                return f"Hello Mrs {self.fname} {self.lname}. [{str(40 - self.age).zfill(2)}] Years To Reach 40"
+
+user_one = User("Osama", "Mohamed", 38, "Male")
+user_two = User("Eman", "Omar", 25, "Female")
+
+print(user_one.full_details())
+print(user_two.full_details())
+
+
+#Third
+class Message:
+
+    def print_message() :
+        return "Hello From Class Message"
+
+
+print(Message.print_message())
+
+
+#Fourth
+class Games:
+    def __init__(self, *games):
+            self.games=games
+            game = list(self.games)
+            print(type(game))
+            print(len(game))
+            self.game = game
+
+    def show_games(self) :
+        if len(self.game) > 1 :
+            print("I Have Many Games:")
+            for i in self.game :
+                print(f"-- {i}")
+        elif type(self.game) == int :
+            print(f"I Have {self.game} Game")
+        else:
+            print(f"I Have One Game Called {self.game}")
+
+my_game = Games("Shadow Of Mordor")
+my_games_names = Games(["Ys II", "Ys Oath In Felghana", "YS Origin"])
+my_games_count = Games(80)
+
+my_game.show_games()
+# Ouput
+# I Have One Game Called "Shadow Of Mordor"
+
+my_games_names.show_games()
+# Ouput
+# I Have Many Games:
+# -- Ys II
+# -- Ys Oath In Felghana
+# -- YS Origin
+
+my_games_count.show_games()
+# Output
+# I Have 80 Game.
+
+
+#Fifth
+class Members:
+
+    def __init__(self, n, p):
+
+        self.name = n
+
+        self.permission = p
+
+    def show_info(self):
+
+        return f"Your Name Is {self.name} And You Are {self.permission}"
+
+
+class Admins(Members):
+    def __init__(self, n, p):
+
+        self.name = n
+
+        self.permission = p
+
+    def show_info(self):
+
+        return f"Your Name Is {self.name} And You Are {self.permission}"
+
+
+class Moderators(Admins):
+    def __init__(self, n, p):
+
+        self.name = n
+
+        self.permission = p
+
+    def show_info(self):
+
+        return f"Your Name Is {self.name} And You Are {self.permission}"
+member_one = Admins("Osama", "Admin")
+member_two = Moderators("Ahmed", "Moderator")
+
+print(member_one.show_info())
+
+
+print(member_two.show_info())
+
+
+#Sixth
+class A:
+
+    def __init__(self, one):
+
+        self.one = one
+
+class B:
+
+    def __init__(self, two):
+
+        self.two = two
+
+class C:
+
+    def __init__(self, three):
+
+        self.three = three
+
+class Text(A ,B ,C) :
+    def __init__(self, one , two , three):
+        self.one = one 
+        self.two = two 
+        self.three = three
+    
+    def show_name(self) :
+        return f"The Name Is {self.one}{self.two}{self.three}"
+
+the_name = Text("El", "ze", "ro")
+
+print(the_name.show_name())
+
